c542fbfe389094981abb37ce0445c57dcafe8c34
Joe Thompson <jhthompson12@gmail.com>
2020-05-25 20:23:21 -0700

    added some tariff data and installed requests

--- UtilityRateDataCollector.py
+++ UtilityRateDataCollector.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 
 
 ### Use this plant data to get a mapping of electric utilities to EIA IDs and the states that the utility is in
@@ -16,6 +17,25 @@ rate_db = rate_db[(rate_db["sector"] == "Residential") & (~rate_db["startdate"].
 
 rate_db["eiaid"] = rate_db["eiaid"].astype("int")
 
-rate_db = rate_db.drop(["label", "startdate", "enddate", "sector", "source", "sourceparent"], axis=1)
+rate_db = rate_db.drop(["startdate", "enddate", "sector", "source", "sourceparent"], axis=1)
+rate_db = rate_db[["label", "eiaid", "name", "utility", "basicinformationcomments"]]
 
 rate_db.to_csv("Utility Tariffs/tariff_db.csv", index=False)
+
+# send a request to OpenEI to get the selected rate info
+api_url = "https://api.openei.org/utility_rates?params"
+
+paramets = {
+    "version": "latest",
+    "format": "csv",
+    "api_key": "snVIu7FREbmucvgeZjPgsumRjdCCqQtwHd0QJsmb",
+    "getpage": "5d5582305457a3e01135a086"
+}
+
+tariff_response = requests.get(api_url, params=paramets)
+
+f = open("Utility Tariffs/utility_response.csv", "w")
+f.write(tariff_response.text)
+f.close()
+
+selected_tariff = pd.read_csv("Utility Tariffs/utility_response.csv").dropna(axis=1)
