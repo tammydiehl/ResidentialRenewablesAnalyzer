ccfe34338dd30e09a45a75cc1f4092f0e483d997
Joe Thompson <jhthompson12@gmail.com>
2020-05-28 21:45:17 -0700

    added API call to get detailed utility tariff data

--- UtilityRateDataCollector.py
+++ UtilityRateDataCollector.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import requests
+import json
+
 
 
 ### Use this plant data to get a mapping of electric utilities to EIA IDs and the states that the utility is in
@@ -12,27 +14,34 @@ import requests
 #
 # eia_data.to_csv("Utility Tariffs/utilities_state_mapper.csv", index=False)
 
-rate_db = pd.read_csv("/Users/jhthompson12/Desktop/usurdb.csv")
-rate_db = rate_db[(rate_db["sector"] == "Residential") & (~rate_db["startdate"].isna()) & (rate_db["enddate"].isna())]
+### create a database of utility tariffs from the OpenEI Utility Tariff database. Filter these down and one keep a small amount of info for each. The rest of the data can be acquired on the fly later with a call to the openEI API
+# rate_db = pd.read_csv("/Users/jhthompson12/Desktop/usurdb.csv")
+# rate_db = rate_db[(rate_db["sector"] == "Residential") & (~rate_db["startdate"].isna()) & (rate_db["enddate"].isna())]
+#
+# rate_db["eiaid"] = rate_db["eiaid"].astype("int")
+#
+# rate_db = rate_db.drop(["startdate", "enddate", "sector", "source", "sourceparent"], axis=1)
+# rate_db = rate_db[["label", "eiaid", "name", "utility", "basicinformationcomments"]]
+#
+# rate_db.to_csv("Utility Tariffs/tariff_db.csv", index=False)
 
-rate_db["eiaid"] = rate_db["eiaid"].astype("int")
+rate_db = pd.read_csv("Utility Tariffs/tariff_db.csv")
 
-rate_db = rate_db.drop(["startdate", "enddate", "sector", "source", "sourceparent"], axis=1)
-rate_db = rate_db[["label", "eiaid", "name", "utility", "basicinformationcomments"]]
-
-rate_db.to_csv("Utility Tariffs/tariff_db.csv", index=False)
-
-# send a request to OpenEI to get the selected rate info
+# send a request to OpenEI API to get the selected rate info
 api_url = "https://api.openei.org/utility_rates?params"
 
 paramets = {
     "version": "latest",
-    "format": "csv",
+    "format": "json",
     "api_key": "snVIu7FREbmucvgeZjPgsumRjdCCqQtwHd0QJsmb",
-    "getpage": "5d5582305457a3e01135a086"
+    "getpage": "5d5582305457a3e01135a086",
+    "detail": "full",
+    "limit": 500
 }
 
 tariff_response = requests.get(api_url, params=paramets)
+
+tariff_data = json.loads(tariff_response.text)["items"][0]
 
 f = open("Utility Tariffs/utility_response.csv", "w")
 f.write(tariff_response.text)
